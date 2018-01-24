5456514ab7fd26e0161adb714f729a9876f8e3d1
suzhangjiegh <su2010yx@163.com>
2018-01-24 15:33:04 +0800

    添加ajax

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,10 @@ import { QRScanner } from '@ionic-native/qr-scanner';
 import { HTTP } from '@ionic-native/http';
 
 import {Config} from "../config/index";
+
+import {HttpClientModule} from '@angular/common/http';
 import {HttpService} from "../service/httpService";
-
-
+import {HttpService2} from "../service/httpService2";
 
 @NgModule({
 	declarations: [
@@ -21,6 +22,7 @@ import {HttpService} from "../service/httpService";
 	],
 	imports: [
 		BrowserModule,
+		HttpClientModule,
 		IonicModule.forRoot(MyApp),
 	],
 	bootstrap: [IonicApp],
@@ -30,7 +32,7 @@ import {HttpService} from "../service/httpService";
 	providers: [
 		StatusBar,
 		SplashScreen,
-		Camera,Config,QRScanner,HttpService,HTTP,
+		Camera,Config,QRScanner,HttpService,HTTP,HttpClientModule,HttpService2,
 		{provide: ErrorHandler, useClass: IonicErrorHandler},
 	]
 })
