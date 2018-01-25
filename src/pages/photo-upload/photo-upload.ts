599c90869aea4d62a29d98762163b050be47243b
suzhangjiegh <su2010yx@163.com>
2018-01-25 16:34:24 +0800

    修改readme

--- src/pages/photo-upload/photo-upload.ts
+++ src/pages/photo-upload/photo-upload.ts
@@ -6,20 +6,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
+ *
+ *
+ * html5 no test
+ * <input type="file" accept="video/*;capture=camcorder">
+ <input type="file" accept="audio/*;capture=microphone">
+ <input type="file" accept="image/*;capture=camera">直接调用相机（测试安卓可以，iphone还是有相册）
+ <input type="file" accept="image/*" />调用相机 图片或者相册
+ <input type="file" multiple accept="image/*" />调用相册
  */
 
 @IonicPage()
 @Component({
-  selector: 'page-photo-upload',
-  templateUrl: 'photo-upload.html',
+	selector: 'page-photo-upload',
+	templateUrl: 'photo-upload.html',
 })
 export class PhotoUploadPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	constructor(public navCtrl: NavController, public navParams: NavParams) {
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PhotoUploadPage');
-  }
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad PhotoUploadPage');
+	}
 
+	file(){
+		console.log("hello world")
+	}
 }
