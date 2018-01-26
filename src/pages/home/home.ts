7272373961664f52af7c2504ae3fd4131bd60006
suzhangjiegh <su2010yx@163.com>
2018-01-26 15:51:25 +0800

    修改treeview 完成 添加增加和修改功能

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {IonicPage, NavController} from 'ionic-angular';
-import {TreeViewPage} from "../tree-view/tree-view";
 
 /**
  * Generated class for the HomePage page.
--- src/pages/tree-view/item-view.ts
+++ src/pages/tree-view/item-view.ts
@@ -1,19 +1,50 @@
-import {Component, Input, OnInit} from "@angular/core";
+import {Component, Input} from "@angular/core";
 
 @Component({
 	selector: 'item-view',
 	template:`<li>
-		<div>{{ item.name}}</div>
-		<ul>
-			<item *ngFor="let items of item.children" [item]="items"></item>
+		<div [ngClass]="{bold:open}" (click)="toggle()">{{ item.name}}
+			<span *ngIf="isFolder">  {{open?'-' : '+'}}</span>
+			<!--<span (click)="del(i)">  删除 {{index}}</span>-->
+		</div>
+		<ul *ngIf="isFolder && open">
+			<item-view *ngFor="let it of item.children;let i = index" [item]="it" [i]="i" [parent]="item" class="item"></item-view>
+			<li class="add" (click)="addChild()">+</li>
 		</ul>
 	</li>`
 })
-export class ItemView implements OnInit{
+export class ItemView{
 
 	@Input() item:any;
+	@Input() i:any;
+	@Input() parent:any;
 
-	ngOnInit(): void {
-		console.log(this.item);
+	private open:boolean =false;
+
+	// 识别是否有下级
+	isFolder(){
+		return this.item.children &&
+			this.item.children.length
+	}
+
+	// 展开子菜单
+	toggle(){
+		if (this.isFolder) {
+			this.open = !this.open
+		}
+	}
+
+	addChild(){
+		if (this.item.children ==undefined){
+			this.item.children=[];
+		}
+
+		this.item.children.push({
+			name: 'new stuff'
+		})
+	}
+
+	del(i){
+		this.parent.children.splice(i,1);
 	}
 }
